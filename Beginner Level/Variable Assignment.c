bdb53a8617ef4f2345c0c5da1c03a69225deb0a1
15EUCS138 <34640771+15EUCS138@users.noreply.github.com>
2017-12-18 15:09:43 +0530

    Create Variable Assignment.c

---
+++ Beginner Level/Variable Assignment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{	
+char a[30]="Sachin Ramesh Tendulkar";    
+char b[20]="Little Master";    
+int r=18426;    
+float avg=44.83;    
+int m=463;    
+printf("%s\n",a);	
+printf("%s\n",b);	
+printf("%d\n",r);	
+printf("%.2f\n",avg);	
+printf("%d\n",m);
+}
