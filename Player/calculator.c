b921a5dadd8ed148f528f464f88c53072a7ec568
15EUCS138 <34640771+15EUCS138@users.noreply.github.com>
2017-12-19 12:53:46 +0530

    Create calculator.c

---
+++ Player/calculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{	
+int a,b,c;	
+printf("Enter the values:");	
+scanf("%d%d",&a,&b);	
+c=a+b;	
+printf("sum %d\n",c);	
+c=a-b;	
+printf("subtract %d\n",c);	
+c=a*b;	
+printf("Multiply %d\n",c);	
+c=a/b;	
+printf("divide %d\n",c);	
+c=a%b;	
+printf("modulus %d\n",c);
+}
