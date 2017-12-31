d3a17d40dafe69d5cb8735447bd541e18872eede
15EUCS138 <34640771+15EUCS138@users.noreply.github.com>
2017-12-31 18:17:08 +0530

    Create typ.c

---
+++ Hunter/typ.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+typedef struct Hotel
+{
+int room[30];
+char name[50];
+int date;
+int occ_no;
+int ent_date;
+}rooms;
+int main()
+{
+	rooms vacant;
+ printf("room vacants:\n");
+ printf("23,65,43,23,22");
+}
