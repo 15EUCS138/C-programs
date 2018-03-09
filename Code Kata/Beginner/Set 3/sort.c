0e3a8248796ef7d68c97a46e2b48e212d067e389
15EUCS138 <34640771+15EUCS138@users.noreply.github.com>
2018-03-09 21:25:05 +0530

    Create sort.c

---
+++ Code Kata/Beginner/Set 3/sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100];
+    int n,i,j,tmp;
+    scanf("%d", &n);
+	printf("enter the elements in a array :\n");
+    for(i=0;i<n;i++)
+    {
+	      scanf("%d",&a[i]);
+	}
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    printf("The sorted elements are:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+}
