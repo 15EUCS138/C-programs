77745e2c2a704fca2708da50edad3f758badcf95
15EUCS138 <34640771+15EUCS138@users.noreply.github.com>
2018-04-02 15:54:00 +0530

    Update and rename fibbo.c to fibbo1.c

--- Code Kata/Beginner/Set 4/fibbo1.c
+++ Code Kata/Beginner/Set 4/fibbo1.c
@@ -4,13 +4,13 @@ int main(void) {
 	int a=0,b=1,c,n,i;
 	printf("Enter the number:");
 	scanf("%d",&n);
-	printf("Fibbonacci series:");
-	for(i=1;i<=n;i++)
+	printf("\n%d %d",a,b); 
+	for(i=1;i<=n-2;i++)
 	{
 		c=a+b;
 		a=b;
 		b=c;
-		printf("%d\n",a);
+		printf(" %d",c);
 	}
 	return 0;
 }
